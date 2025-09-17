fe7c503035272fb2d2393829d1ff042c0633ecb1
Lutsyuk Victoria <victorialutsyuk.t@gmail.com>
2025-09-17 14:02:57 +0300

    chore: add arch

---
+++ app/src/main/java/com/vicitori/application/parallel/strategies/GridStrategy.java
@@ -0,0 +1,4 @@
+package main.java.com.vicitori.application.parallel.strategies;
+
+public class GridStrategy {
+}
---
+++ app/src/main/java/com/vicitori/domain/filters/CompositeFilter.java
@@ -0,0 +1,4 @@
+package main.java.com.vicitori.domain.filters;
+
+public class CompositeFilter {
+}
