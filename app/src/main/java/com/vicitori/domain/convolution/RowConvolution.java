08562185a88a0a314545c1c08f4e1dcca2aa6055
Lutsyuk Victoria <victorialutsyuk.t@gmail.com>
2025-09-18 22:24:51 +0300

    feat: add templates for other convolutions

---
+++ app/src/main/java/com/vicitori/domain/convolution/RowConvolution.java
@@ -0,0 +1,12 @@
+package com.vicitori.domain.convolution;
+
+import com.vicitori.domain.filters.FilterProfile;
+
+import java.awt.image.BufferedImage;
+
+public class RowConvolution implements Convolution {
+    @Override
+    public BufferedImage apply(BufferedImage input, FilterProfile filter) {
+        throw new UnsupportedOperationException("Sequential convolution not implemented yet.");
+    }
+}
