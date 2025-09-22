a2c817679df627ba3d642f847a2b03035175da72
Lutsyuk Victoria <victorialutsyuk.t@gmail.com>
2025-09-22 03:54:38 +0300

    feat: add more interesting filters

--- app/src/main/java/com/vicitori/core/filters/FiltersLibrary.java
+++ app/src/main/java/com/vicitori/core/filters/FiltersLibrary.java
@@ -7,16 +7,7 @@ import java.util.Set;
 
 public class FiltersLibrary {
     private static final Map<String, Filter> FILTERS = Map.of(
-            "blur", new Filter(
-                    new float[][]{
-                            {0.0f, 0.0f, 1.0f, 0.0f, 0.0f},
-                            {0.0f, 1.0f, 1.0f, 1.0f, 0.0f},
-                            {1.0f, 1.0f, 1.0f, 1.0f, 1.0f},
-                            {0.0f, 1.0f, 1.0f, 1.0f, 0.0f},
-                            {0.0f, 0.0f, 1.0f, 0.0f, 0.0f}
-                    },
-                    1f / 13, 0.0f
-            ), "motion_blur", new Filter(
+            "motion_blur", new Filter(
                     new float[][]{
                             {1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f},
                             {0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f},
@@ -28,7 +19,27 @@ public class FiltersLibrary {
                             {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f},
                             {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f},
                     }, 1.0f / 9, 0.0f
-            ), "find_edges", new Filter(
+            ), "radial_blur", new Filter(
+                    new float[][]{
+                            {0.0f, 0.1f, 0.0f},
+                            {0.1f, 0.6f, 0.1f},
+                            {0.0f, 0.1f, 0.0f}
+                    }, 1.0f, 0.0f
+            ),"blur", new Filter(
+                    new float[][]{
+                            {1.0f, 4.0f, 6.0f, 4.0f, 1.0f},
+                            {4.0f, 16.0f, 24.0f, 16.0f, 4.0f},
+                            {6.0f, 24.0f, 36.0f, 24.0f, 6.0f},
+                            {4.0f, 16.0f, 24.0f, 16.0f, 4.0f},
+                            {1.0f, 4.0f, 6.0f, 4.0f, 1.0f}
+                    }, 1.0f / 256, 0.0f
+            ),"emboss", new Filter(
+                    new float[][]{
+                            {-2.0f, -1.0f, 0.0f},
+                            {-1.0f, 1.0f, 1.0f},
+                            {0.0f, 1.0f, 2.0f}
+                    }, 0.7f, 128.0f
+            ),"find_edges", new Filter(
                     new float[][]{
                             {-1.0f, 0.0f, 0.0f, 0.0f, 0.0f},
                             {0.0f, -2.0f, 0.0f, 0.0f, 0.0f},
@@ -37,17 +48,24 @@ public class FiltersLibrary {
                             {0.0f, 0.0f, 0.0f, 0.0f, -1.0f},
 
                     }, 1.0f, 0.0f
-            ), "sharpen", new Filter(
+            ),"glass_distortion", new Filter(
                     new float[][]{
-                            {-1.0f, -1.0f, -1.0f, -1.0f, -1.0f,},
-                            {-1.0f, 2.0f, 2.0f, 2.0f, -1.0f,},
-                            {-1.0f, 2.0f, 8.0f, 2.0f, -1.0f,},
-                            {-1.0f, 2.0f, 2.0f, 2.0f, 0.0f},
-                            {-1.0f, -1.0f, -1.0f, -1.0f, -1.0f},
-
-                    }, 1.0f / 8, 0.0f
-            )
-    );
+                            {0.0f, 1.0f, 0.0f},
+                            {1.0f, -4.0f, 1.0f},
+                            {0.0f, 1.0f, 0.0f}
+                    }, 1.5f, 128.0f
+            ),"negative", new Filter(
+                    new float[][]{
+                            {-1.0f}
+                    }, 0.9f, 255.0f
+            ),"pixelate", new Filter(
+                    new float[][]{
+                            {1.0f, 1.0f, 0.0f, 0.0f},
+                            {1.0f, 1.0f, 0.0f, 0.0f},
+                            {0.0f, 0.0f, 1.0f, 1.0f},
+                            {0.0f, 0.0f, 1.0f, 1.0f}
+                    }, 0.15f, 0.0f
+            ));
 
     public static Filter get(String name) {
         return FILTERS.get(name.toLowerCase());
